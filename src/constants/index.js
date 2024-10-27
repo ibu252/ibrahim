4b57707d1b9ff0b8b42cb8ddd1d04cef7aa5306a
ibu252 <na21b046@smail.iitm.ac.in>
2024-10-27 20:14:42 +0530

    my_website

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Robot Control",
+    icon: web,
+  },
+  {
+    title: "Robot Path Planning",
+    icon: mobile,
+  },
+  {
+    title: "Robot Simulator",
+    icon: backend,
+  },
+  {
+    title: "Robot Power train",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Reasech Fellow",
+    company_name: "MAV Laboratory",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "Sept 2023 - Present",
+    points: [
+      "Developing a 50m depth­rated AUV with dual maneuvering modes, including surge and docking with actuators.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed a Browser­based maritime ship simulator, now integrating LIDAR, Camera with sensor noises.",
+      "Developed multi­agent autonomy architecture for ASVs, for testing autonomy algorithms in lake environment.",
+      
+    ],
+  },
+  {
+    title: "Team Lead",
+    company_name: "Team Aritra",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Sept 2022 - Sept 2024",
+    points: [
+      "Led the Aritra competition team as overall team head and Electromechanical Module Head",
+      "managed a 10 member team, and serving as the point of contact for sponsors and mentors during international competitions.",
+      "Designed and developed an autonomous catamaran with a sensor suite, including LiDAR, camera, GPS, IMU, and X­thruster configuration.",
+      "Sponsored by the Royal Norwegian Embassy, IEEE OES Madras Chapter, NIOT, MAV Lab and SBG Sensors.",
+      "Engineered the ASV's entire mechanical module with modular bracket system and electrical powertrain.",
+    ],
+  },
+  {
+    title: "Coordinator",
+    company_name: "Envisage CFI",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Competition Master",
+    company_name: "Aquavision 2024",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "aug 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Development of a High­ Fidelity Multi­Agent Maritime Simulator",
+    name: "OMAE",
+    designation: "2024",
+    company: "Singapore",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Maritime Simulator",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Cooperative Autonomy",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "  Holonomic ASV Development",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
